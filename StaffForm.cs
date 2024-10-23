7e122fc2c0fd4a17126499486cfe94c74b96cf1b
binhcuong1 <binhcuongpy2014@gmail.com>
2024-10-23 19:26:10 +0700

    pussh

--- StaffForm.cs
+++ StaffForm.cs
@@ -1,4 +1,5 @@
 ﻿using QuanLyCuaHang.Ultilities;
+using QuanLyCuaHang.UserControls;
 using QuanLyCuaHang.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,8 @@ namespace QuanLyCuaHang
 {
     public partial class StaffForm : Form
     {
+        string currStaffID = string.Empty;
+
         ConveStoreDBContext db;
 
         List<StaffViewModel> staffList;
@@ -26,10 +29,15 @@ namespace QuanLyCuaHang
             db = new ConveStoreDBContext();
 
             LoadStaffToGrid();
+
+            dgvStaff.CellClick += dgvStaff_CellContentClick;
         }
 
         private void StaffForm_Load(object sender, EventArgs e)
         {
+            // Ẩn panel
+            pnlEditStaff.Visible = false;
+
             BindToGrid();
         }
 
@@ -49,5 +57,79 @@ namespace QuanLyCuaHang
         {
             dgvStaff.DataSource = staffList;
         }
+
+        #region Button Methods
+        private void BtnAddStaff_Click(object sender, EventArgs e)
+        {
+            Ultilities.Services.SetCenterPanel(this, pnlEditStaff);
+
+            pnlEditStaff.Visible = true;
+            pnlEditStaff.BringToFront();
+
+            AddStaffUC addStaffUC = new AddStaffUC(ref pnlEditStaff, ref dgvStaff);
+            pnlEditStaff.Controls.Add(addStaffUC);
+            addStaffUC.Dock = DockStyle.Fill;
+        }
+
+        private void BtnDeleteStaff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(currStaffID))
+                    throw new Exception("Hãy click vào nhân viên cần xóa");
+
+                NHANVIEN staffToDelete = db.NHANVIENs.FirstOrDefault(nv => nv.MaNV == currStaffID);
+
+                if (staffToDelete is null)
+                    throw new Exception("Không tìm thấy nhân viên, hãy làm mới lại bảng");
+                else
+                {
+                    DialogResult resultYesOrNo = MessageBox.Show("Bạn có chắc chắn muốn xóa", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if (resultYesOrNo == DialogResult.Yes)
+                    {
+                        db.NHANVIENs.Remove(staffToDelete);
+                        db.SaveChanges();
+
+                        LoadStaffToGrid();
+                        BindToGrid();
+
+                        throw new Exception("Xóa sản phẩm thành công");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                currStaffID = string.Empty;
+                MessageBox.Show(ex.Message, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            DialogResult dg = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dg == DialogResult.Yes)
+            {
+                HomePageForm homePageForm = new HomePageForm();
+                this.Hide();
+                homePageForm.Show();
+            }
+        }
+        #endregion
+
+        private void dgvStaff_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem dòng hiện tại có hợp lệ không (dòng tiêu đề không được tính)
+            if (e.RowIndex >= 0)
+            {
+                // Lấy giá trị của cột "MaNV" từ dòng được click
+                string ProductID = dgvStaff.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+                currStaffID = ProductID;
+            }
+        }
+
+        
     }
 }
---
+++ UserControls/AddStaffUC.cs
@@ -0,0 +1,124 @@
+﻿using QuanLyCuaHang.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHang.UserControls
+{
+    public partial class AddStaffUC : UserControl
+    {
+        private int numOfStaff = 0;
+
+        ConveStoreDBContext db;
+
+        private Panel currPanel;
+        private DataGridView currDgv;
+
+        public AddStaffUC(ref Panel currPanel, ref DataGridView currDgv)
+        {
+            InitializeComponent();
+
+            db = new ConveStoreDBContext();
+
+            numOfStaff = db.NHANVIENs.Count();
+            
+            this.currPanel = currPanel;
+            this.currDgv = currDgv;
+        }
+
+
+
+        private void AddStaffUC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Button Methods
+        private void btnAddStaff_Click(object sender, EventArgs e)
+        {
+            bool isEmptyInput = string.IsNullOrEmpty(txtStaffName.Text)
+                    || string.IsNullOrEmpty(txtNumberPhone.Text);
+
+            try
+            {
+                if (isEmptyInput)
+                    throw new Exception("Bạn phải chọn đầy đủ các lựa chọn");
+
+
+                NHANVIEN newSfaff = FillInputToEntityProduct();
+                db.NHANVIENs.Add(newSfaff);
+                db.SaveChanges();
+
+                throw new Exception("Thêm nhân viên thành công");
+            }
+            catch (Exception ex)
+            {
+                ResetInput();
+                MessageBox.Show(ex.Message, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Ẩn UserControl
+            this.Visible = false;
+
+            // Ẩn panel chứa UserControl
+            if (currPanel != null)
+            {
+                currPanel.Visible = false; // Ẩn panel
+            }
+
+            
+        }
+        #endregion
+
+
+        #region Other Methods
+        // Phương thức đổ input vào thực thể nhân viên mới
+        private NHANVIEN FillInputToEntityProduct()
+        {
+            NHANVIEN result = new NHANVIEN();
+
+            result.MaNV = CreateNewStaffID();
+            result.TenNV = txtStaffName.Text;
+            result.DiaChi = txtNumberPhone.Text;
+            result.NgayVaoLam = DateTime.Today;
+
+            return result;
+        }
+
+        // Phương thức tạo mới 1 mã nhân viên
+        private string CreateNewStaffID()
+        {
+            string result = string.Empty;
+            bool isTwoDigitNumber = numOfStaff <= 98 && numOfStaff >= 9;
+            bool isThreeDigitNumber = numOfStaff <= 998 && numOfStaff >= 99;
+
+            numOfStaff += 2;
+
+            if (isTwoDigitNumber)
+                result = "NV0" + numOfStaff.ToString().Trim();
+            else if (isThreeDigitNumber)
+                result = "NV" + numOfStaff.ToString().Trim();
+
+            return result;
+        }
+
+        // Phương thức reset lại các giá trị input
+        private void ResetInput()
+        {
+            txtAddress.Text = string.Empty;
+            txtNumberPhone.Text = string.Empty;
+            txtStaffName.Text = string.Empty;
+        }
+        #endregion
+
+    }
+}
